adaf06c247c2e6abbad11e6707e127dfcf893f3f
Jhony Juarez <jhonyrbj@gmail.com>
2021-05-25 14:47:45 -0300

    Cracao da tabela Empresa.

---
+++ src/main/resources/db/create-tabela-empresa.sql
@@ -0,0 +1,10 @@
+create table Empresa(
+    cnpj varchar(14) not null unique,
+    nome varchar(256) not null,
+    email varchar(256) not null unique,
+    senha varchar(256) not null,
+    cidade varchar(256) not null,
+    descricao varchar(256),
+
+    primary key (cnpj)
+);
