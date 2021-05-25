862568fa0bf2573993b22954b0c8d543ce415115
Alvaro Magalhães <alvpmagalhaes@gmail.com>
2021-05-25 15:24:02 -0300

    Create GenericDAO

---
+++ src/main/java/br/ufscar/dc/dsw/dao/GenericDAO.java
@@ -0,0 +1,21 @@
+package br.ufscar.dc.dsw.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class GenericDAO {
+    
+    public GenericDAO() {
+        try {
+        	Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected Connection getConnection() throws SQLException {
+    	String url = "jdbc:mysql://localhost:3306/rh?useTimezone=true&serverTimezone=UTC";  	
+    	return DriverManager.getConnection(url, "root", "root");
+    }
+}
