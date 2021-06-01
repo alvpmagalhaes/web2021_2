305b0ca23fa6d182a0c3c82fd2d66f877ad8a021
Jhony Juarez <jhonyrbj@gmail.com>
2021-06-01 14:12:12 -0300

    Criando controller de listagem de vagas.

--- src/main/resources/db/create-tabela-vaga.sql
+++ src/main/resources/db/create-tabela-vaga.sql
@@ -4,5 +4,7 @@ create table Vaga(
     descricao varchar(256) not null,
     remuneracao varchar(256) not null,
     codigo bigint not null,
+    cnpjEmpresa varchar(14) not nul,
+    foreign key (cnpjEmpresa) references Empresa(cnpj),
     primary key (codigo)
 );
