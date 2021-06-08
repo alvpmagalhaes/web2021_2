9ff582a27196af6cb41ca08be36fbbce45030325
Jhony Juarez <jhonyrbj@gmail.com>
2021-06-08 10:56:52 -0300

    Criando artefatos para a Ativdade AA2.

---
+++ Atividade AA-2/src/main/java/br/ufscar/dc/dsw/atividadeaa2/Atividadeaa2Application.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.atividadeaa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividadeaa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividadeaa2Application.class, args);
+	}
+
+}
